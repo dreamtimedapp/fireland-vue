6f40b2689986faca3fc0520ebf71fa568a800fcd
tianling.lk <tianling.lk@alibaba-inc.com>
2018-10-29 19:19:54 +0800

    修复自动化脚本的构建错误

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 # 引用工程路径
-PROJECT_PATH = /home/ubuntu/fireland-vue
+PROJECT_PATH=/home/ubuntu/fireland-vue
 
 # 工程构建完成后存放的路径
-BUILD_PATH = ${PROJECT_PATH}/dist/
+BUILD_PATH=${PROJECT_PATH}/dist/
 
 # 页面部署在Nginx上的路径
-NGINX_DEPLOY_PATH = /home/ubuntu/www/
+NGINX_DEPLOY_PATH=/home/ubuntu/www/
 
 cd  ${PROJECT_PATH}
 
