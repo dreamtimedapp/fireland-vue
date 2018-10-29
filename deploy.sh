17661f17f54ce125992d071e0360947c746a0861
tianling.lk <tianling.lk@alibaba-inc.com>
2018-10-29 18:46:15 +0800

    添加自动化部署脚本

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+# 引用工程路径
+PROJECT_PATH = /home/ubuntu/fireland
+
+# 工程构建完成后存放的路径
+BUILD_PATH = ${PROJECT_PATH}/dist/
+
+# 页面部署在Nginx上的路径
+NGINX_DEPLOY_PATH = /home/ubuntu/www/
+
+
+git checkout develop
+
+git pull origin develop
+
+npm install $PROJECT_PATH 
+
+npm run build $PROJECT_PATH
+
+# 删除旧构建文件
+rm -r ${NGINX_DEPLOY_PATH}*
+
+cp -R ${BUILD_PATH}* $NGINX_DEPLOY_PATH
+
