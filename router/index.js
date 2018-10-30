353be605d1cb343fef5d6cc6a0f0e3c92bed452b
tianling.lk <tianling.lk@alibaba-inc.com>
2018-10-30 12:22:45 +0800

    修复跨域问题

--- router/index.js
+++ router/index.js
@@ -2,15 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8");
-  if (req.method == 'OPTIONS') {
-    res.send(200); /*让options请求快速返回*/
-  } else {
-    next();
-  }
+    res.send('Hello Express!')
 })
 module.exports = router
