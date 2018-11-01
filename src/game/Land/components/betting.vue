f7a39d5ba78ecc62020de38924e6c3c27337a814
tianling.lk <tianling.lk@alibaba-inc.com>
2018-11-01 20:17:36 +0800

    天

--- src/game/Land/components/betting.vue
+++ src/game/Land/components/betting.vue
@@ -149,10 +149,7 @@ export default {
           }
        },
        async withdraw() {
-          if (store.state.LandStore.game_balance == '') {
-              alert('请输入提现金额')
-              return
-          } 
+          
           let res = await withdraw(store.state.LandStore.account_name,CONTRACT_NAME,parseFloat(store.state.LandStore.game_balance).toFixed(4),'EOS');
           if (res.is_error) {
             alert(JSON.stringify(res.msg))
